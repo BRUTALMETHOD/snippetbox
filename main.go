3c753b9cf86c8a816d4a6b1b634cc2c84613085c
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-12-30 11:55:11 +0800

    Chapter 2.5

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// default catch all
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	_, err := w.Write([]byte("Hello from Snippetbox"))
+	if err != nil {
+		log.Printf("ResponseWriter failed: %v", err)
+	}
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	_, err := w.Write([]byte("Create a new snippet..."))
 	if err != nil {
 		log.Printf("ResponseWriter failed: %v", err)
 	}
@@ -15,6 +47,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
